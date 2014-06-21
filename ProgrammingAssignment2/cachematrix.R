69a54033aa4af4247eaa779de095308e33201133
gabx <arnaud.gaboury@gmail.com>
2014-06-22 00:02:21 +0200

    progress in writing makeCacheMatrix

--- ProgrammingAssignment2/cachematrix.R
+++ ProgrammingAssignment2/cachematrix.R
@@ -1,9 +1,15 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+# function to inverse a matrix then cache the result as a matrix object. We 
+# assume the original matrix is invertible
 
 makeCacheMatrix <- function(x = matrix()) {
+    
+    # create a new env with empty parent. 
+    my_env <- new.env(parent = emptyenv())
+    # store m variable in my_env & bind it to NULL
+    my_env$m <- NULL
+    
+ 
 
 }
 
