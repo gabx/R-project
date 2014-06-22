8e1afce4bb61807c457d159671c94f6ede238303
gabx <arnaud.gaboury@gmail.com>
2014-06-22 10:59:07 +0200

    first function seems OK

--- ProgrammingAssignment2/cachematrix.R
+++ ProgrammingAssignment2/cachematrix.R
@@ -1,17 +1,32 @@
 
-# function to create a matrix, set/get its value and set/get its inverse 
-# with the solve() function. 
-# We will assume the matrix is inversible
+# function to set/get the value of a matrix and set/get its inverse 
+# with the solve() function, as a list of functions.
+# We will assume the matrix is inversible (i.e nrow=ncol)
+# This kind of functions are called "constructor".
 
 makeCacheMatrix <- function(x = matrix()) {
     
     # create a new env with empty parent. This avoid inherit unwanted objects
     my_env <- new.env(parent = emptyenv())
-    # store m variable in my_env & bind it to NULL
-    my_env$m <- NULL
+    # store variable s in my_env & bind it to NULL
+    my_env$s <- NULL
+
     
+    set <- function(y){
+        # this fun assign y to x & NULL to s
+        x <<- y
+        s <<- NULL 
+    }
+    
+    # return x
     get <- function() x
+    
+    setsolve <- function(solve) s <<- solve
+    
+    # return y, inverse of x
+    getsolve <- function() i
  
+    list(set = set, get = get, setsolve = setsolve, getsolve = getsolve)
 
 }
 
